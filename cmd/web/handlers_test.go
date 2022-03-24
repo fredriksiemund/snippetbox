e86602071af77241da078bcca65d786868e6bc0b
Fredrik Siemund <fredrik.siemund@gmail.com>
2022-03-24 15:35:45 +0100

    Test showSnippet

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,39 +1,12 @@
 package main
 
 import (
-	"io"
+	"bytes"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
-func TestPingUnit(t *testing.T) {
-	rr := httptest.NewRecorder()
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ping(rr, r)
-
-	rs := rr.Result()
-
-	if rs.StatusCode != http.StatusOK {
-		t.Errorf("expected %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if string(body) != "OK" {
-		t.Errorf("expected %s; got %s", "OK", string(body))
-	}
-}
-
-func TestPingEndToEnd(t *testing.T) {
+func TestPing(t *testing.T) {
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
 	defer ts.Close()
@@ -45,5 +18,46 @@ func TestPingEndToEnd(t *testing.T) {
 	}
 	if string(body) != "OK" {
 		t.Errorf("expected %s; got %s", "OK", string(body))
+	}
+}
+
+func TestShowSnippet(t *testing.T) {
+	// Create a new instance of our application struct which uses the mocked
+	// dependencies.
+	app := newTestApplication(t)
+
+	// Establish a new test server for running end-to-end tests.
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	// Set up some table-driven tests to check the responses sent by our
+	// application for different URLs.
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contain %q", tt.wantBody)
+			}
+		})
 	}
 }
